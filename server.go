bdc7d8b14d3272854d1219394d0e58020d5be6b0
Matthew Shiel <mattshiel@gmail.com>
2017-11-18 01:07:29 +0000

    Created basic web server to host files

---
+++ server.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	// Serve the entire "web" folder
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	http.ListenAndServe(":8081", nil)
+}
