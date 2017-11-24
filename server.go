f0bb485cf8d40503bdd835dac21a31b3c8b4a304
Matthew Shiel <mattshiel@gmail.com>
2017-11-24 19:26:50 +0000

    Adapted existing Eliza method from KennySong to the webserver

--- server.go
+++ server.go
@@ -10,19 +10,18 @@ import (
 func inputhandler(w http.ResponseWriter, r *http.Request) {
 	question := r.URL.Query().Get("value") // Extract question from GET request
 
-	// Create Eliza's response
-	answer := eliza.GenerateAnswer(question)
-
-	// Return the response
-	fmt.Fprintf(w,"%s",answer)
+	// Return Eliza's response's so long as the user doesn't give a quit statement
+		answer := eliza.ReplyTo(question)
+		
+		// Return Eliza's answer
+		fmt.Fprintf(w,"%s",answer)
 }
 
 func main() {
-
 	// Adapted from https://ianmcloughlin.github.io
 	fs := http.FileServer(http.Dir("web"))
 	http.Handle("/", fs)
 
 	http.HandleFunc("/user-input", inputhandler)
-	http.ListenAndServe(":8083", nil)
+	http.ListenAndServe(":8086", nil)
 }
